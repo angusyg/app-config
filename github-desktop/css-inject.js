5a9b7ee60db2b331a80ef7450d57c4af0c6c189f
Angus.yg <gael.clair@gmail.com>
2018-02-17 23:42:53 +0100

    init

--- github-desktop/css-inject.js
+++ github-desktop/css-inject.js
@@ -8,8 +8,8 @@
 
 var html_file = require("path").join(process.resourcesPath, "app/index.html");
 var html = require("fs").readFileSync(html_file, "utf8");
-var link_to_inject = '<link href="https://cdn.rawgit.com/Yuudaari/83afaa0e1cf21d6852f5349b10e7eb3a/raw/437fdc42d77d25e4f35b91637ce2396f8efa5547/desktop-dark.css" rel="stylesheet">';
-var link_regex = /<link href="https:\/\/cdn\.rawgit\.com\/Yuudaari.*?" rel="stylesheet">/;
+var link_to_inject = '<link href="https://cdn.rawgit.com/angusyg/app-config/9fec9d14/github-desktop/angus-dark.css" rel="stylesheet">';
+var link_regex = /<link href="https:\/\/cdn\.rawgit\.com\/angusyg.*?" rel="stylesheet">/;
 html = html.replace(link_regex, "").replace("</head>", `\n${link_to_inject}</head>`);
 require("fs").writeFileSync(html_file, html, "utf8");
 location.reload();
